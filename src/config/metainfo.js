e6f3dfa7ef5c81b94647e312d3a15cba9c0f84f1
yussan <yussandeveloper@gmail.com>
2018-08-04 13:25:08 +0700

    force update metea

--- src/config/metainfo.js
+++ src/config/metainfo.js
@@ -1,4 +1,4 @@
 export default {
-  title: 'Xiaomiverse - Everythong about Xiaomi',
+  title: 'Xiaomiverse - Everything about Xiaomi',
   description: 'Find various updates about Xiaomi products that have been released on the market. We will also review some of these products for you.'
 }
